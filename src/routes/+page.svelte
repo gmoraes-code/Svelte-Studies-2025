37fccfba4e31138a3b44852087db1b438f0e73ea
Gabriel <gabri.moraes.2011@gmail.com>
2026-01-02 23:53:05 -0300

    lesser tweaks on the sv app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 </head>
 
 <body>
-    <div class = '"mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10'>
+    <div class = "mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
         <h1>This is a new project!</h1>
     </div>
 </body>
