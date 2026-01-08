17b760f94abcbd4f6c68759308d7cdc8b5187010
Gabriel <gabri.moraes.2011@gmail.com>
2026-01-07 23:31:31 -0300

    some small changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 <button 
     class="bg-amber-50 p-4 border" 
     onclick={() => console.log("Hello, World! That's a test")}>
-    <div class="rounded-full flex px-4 py-2 bg-white shadow-sm">
+    <div class="rounded-full flex px-4 py-2 bg-black shadow-sm">
         <h1 class="font-bold">Click Me!</h1>
     </div>
 </button>
